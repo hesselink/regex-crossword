b7457642551ae9242cd05a48f5fa9f21d967054e
Erik Hesselink <hesselink@gmail.com>
2013-02-15 18:04:08 +0100

    Add intersection for regexs.

--- src/Crossword/FixedRegex.hs
+++ src/Crossword/FixedRegex.hs
@@ -1,5 +1,6 @@
 module Crossword.FixedRegex where
 
+import Control.Monad
 import Data.Set (Set, (\\))
 import Crossword.Token
 
@@ -26,8 +27,14 @@ pprAtom (NoneOf toks) = "[^" ++ map unToken (Set.toList toks) ++ "]"
 merges :: [FixedRegex] -> FixedRegex
 merges = foldr1 merge
 
+intersects :: [FixedRegex] -> Maybe FixedRegex
+intersects = foldM1 intersect
+
 merge :: FixedRegex -> FixedRegex -> FixedRegex
 merge = zipWith mergeAtom
+
+intersect :: FixedRegex -> FixedRegex -> Maybe FixedRegex
+intersect = zipWithM intersectAtom
 
 mergeAtom :: Atom -> Atom -> Atom
 mergeAtom Any            _            = Any
@@ -43,6 +50,27 @@ mergeAtom (NoneOf ts1)   (NoneOf ts2) = noneOf (Set.intersection ts1 ts2)
 mergeAtom (NoneOf ts)    (Literal t)  = noneOf (Set.delete t ts)
 mergeAtom (NoneOf ts2)   (OneOf ts1)  = noneOf (ts2 \\ ts1)
 
+intersectAtom :: Atom -> Atom -> Maybe Atom
+intersectAtom Any            a             = Just a
+intersectAtom a              Any           = Just a
+intersectAtom l@(Literal t1) (Literal t2) | t1 == t2  = Just l
+                                          | otherwise = Nothing
+intersectAtom l@(Literal t)  (OneOf ts)    = if t `Set.member` ts then Just l  else Nothing
+intersectAtom l@(Literal t)  (NoneOf ts)   = if t `Set.member` ts then Nothing else Just l
+intersectAtom (OneOf ts)     l@(Literal t) = if t `Set.member` ts then Just l  else Nothing
+intersectAtom (OneOf ts1)    (OneOf ts2)   = oneOf (Set.intersection ts1 ts2)
+intersectAtom (OneOf ts1)    (NoneOf ts2)  = oneOf (ts1 \\ ts2)
+intersectAtom (NoneOf ts1)   (NoneOf ts2)  = Just (NoneOf (Set.union ts1 ts2))
+intersectAtom (NoneOf ts)    l@(Literal t) = if t `Set.member` ts then Nothing else Just l
+intersectAtom (NoneOf ts2)   (OneOf ts1)   = oneOf (ts1 \\ ts2)
+
+oneOf :: Set Token -> Maybe Atom
+oneOf ts =
+  case Set.toList ts of
+    []  -> Nothing
+    [t] -> Just (Literal t)
+    _   -> Just (OneOf ts)
+
 noneOf :: Set Token -> Atom
 noneOf ts =
   case Set.size ts of
@@ -53,3 +81,8 @@ addToken :: Eq a => a -> [a] -> [a]
 addToken x [] = [x]
 addToken x l@(x':xs) | x == x'   = l
                      | otherwise = x' : addToken x xs
+
+foldM1             :: (Monad m) => (a -> a -> m a) -> [a] -> m a
+foldM1 _ []       = error "Empty list in foldM1."
+foldM1 _ [x]      = return x
+foldM1 f (x:y:xs) =  f x y >>= \fax -> foldM1 f (fax:xs)
